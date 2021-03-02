a56ad432673eeb3d0634cec950d80ec6aec681cd
luorenjie <lrj193927@gmail.com>
2021-03-02 09:47:07 -0500

    create ClientService Sync

---
+++ ClientServiceSync/src/main/java/com/antra/ClientServiceSync/ClientServiceSyncApplication.java
@@ -0,0 +1,13 @@
+package com.antra.ClientServiceSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServiceSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServiceSyncApplication.class, args);
+    }
+
+}
