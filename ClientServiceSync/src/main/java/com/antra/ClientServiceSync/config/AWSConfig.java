4a0cff312a6bfd5eef722ef78cb94794a37237a1
luorenjie <lrj193927@gmail.com>
2021-03-07 08:47:43 -0500

    add messageConverter to SQS

---
+++ ClientServiceSync/src/main/java/com/antra/ClientServiceSync/config/AWSConfig.java
@@ -0,0 +1,42 @@
+package com.antra.ClientServiceSync.config;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.aws.core.env.ResourceIdResolver;
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.annotation.support.PayloadArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+
+import java.util.Collections;
+
+@Configuration
+public class AWSConfig {
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate(
+            AmazonSQSAsync amazonSQSAsync, MessageConverter messageConverter) {
+        return new QueueMessagingTemplate(amazonSQSAsync, (ResourceIdResolver) null, messageConverter);
+    }
+
+    @Bean
+    public QueueMessageHandlerFactory queueMessageHandlerFactory(MessageConverter messageConverter) {
+        QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
+        factory.setArgumentResolvers(Collections.<HandlerMethodArgumentResolver>singletonList(new PayloadArgumentResolver(messageConverter)));
+        return factory;
+    }
+
+    @Bean
+    protected MessageConverter messageConverter(ObjectMapper objectMapper) {
+        var converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(objectMapper);
+        converter.setSerializedPayloadClass(String.class);
+        converter.setStrictContentTypeMatch(false);
+        return converter;
+    }
+
+}
+
